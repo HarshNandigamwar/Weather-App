0bdfb8ebdcd3be07d70a5f47b8395eae29597175
Shriharsh Nandigamwar <nandigamwarharsh@gmail.com>
2025-03-19 18:00:04 +0530

    Adding sum more features

--- main.js
+++ main.js
@@ -1,50 +1,51 @@
-const apiKey = "Your api key ";
-    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+//const apiKey = "47e1ddb062be9f09838d5c20310bdf5f";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-    const searchBox = document.querySelector(".search input");
-    const searchBtn = document.querySelector(".search button");
-    const weatherIcon = document.querySelector(".weather-icon");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+let weatherIcon = document.querySelector(".weather-icon");
 
-    async function checkweather(city){
-         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-         
-         if(response.status == 404){
-            document.querySelector(".error").style.display = "block";
-            alert("*Invalid city name");
-            document.querySelector(".weather").style.display = "none";
-         }else {
+async function checkweather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-            var data = await response.json();
-         
-         document.querySelector(".city").innerHTML = data.name;
-         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-        
-         if(data.weather[0].main == "Clouds"){
-                weatherIcon.src = "images/clouds.png";
-         }
-         else if(data.weather[0].main == "Clear"){
-                weatherIcon.src = "images/clear.png";
-         }
-         else if(data.weather[0].main == "Rain"){
-                weatherIcon.src = "images/rain.png";
-         }
-         else if(data.weather[0].main == "Drizzle"){
-                weatherIcon.src = "images/drizzle.png";
-         }
-         else if(data.weather[0].main == "Mist"){
-                weatherIcon.src = "images/mist.png";
-         }else if(data.weather[0].main == "Snow"){
-            weatherIcon.src = "images/snow.png";
-         }
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    alert("*Invalid city name");
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-         document.querySelector(".weather").style.display = "block";
-         document.querySelector(".error").style.display = "none";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
-        }
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+      document.body.style.backgroundImage = "url('./images/Cloud.jpg')";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+      document.body.style.backgroundImage = "url('./images/Clear.jpg')";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+      document.body.style.backgroundImage = "url('./images/Rain.jpg')";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+      document.body.style.backgroundImage = "url('./images/Drizzle.jpg')";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+      document.body.style.backgroundImage = "url('./images/Mist.jpg')";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/snow.png";
+      document.body.style.backgroundImage = "url('./images/Snow.jpg')";
+    }
 
-     }
-    searchBtn.addEventListener("click", ()=>{
-        checkweather(searchBox.value);    
-    })
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+searchBtn.addEventListener("click", () => {
+  checkweather(searchBox.value);
+});
